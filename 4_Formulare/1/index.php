7b397489ca1df7de8533c7a0846dc9ec37743a38
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-04-12 11:19:13 +0200

    Formulare Aufgaben gemacht

---
+++ 4_Formulare/1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aufgabe 1</title>
+</head>
+<body>
+    <h1>Aufgabe 1</h1>
+    <form action="submit" method="post" >
+        Vorname: <input type="text" name="vorname"> <br>
+        Nachname: <input type="text" name="nachname"> <br>
+        <input type="submit" value="Bestätigen">
+    </form>
+</body>
+</html>
+
+
